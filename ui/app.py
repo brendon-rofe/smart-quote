c99067493a56b46ea08975578f9a5aed4099f5f5
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-10 10:59:09 +0200

    feat: Implement inital display of all quotes

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import requests
+import json
 
 st.title("SmartQuote")
+
+with st.spinner():
+  response = requests.get("http://127.0.0.1:8000/quotes")
+  if response.status_code == 200:
+    quotes = response.json()
+    for quote in quotes:
+      st.divider()
+      st.header(f"Customer: {quote["customer"]}")
+      st.write(f"Description: {quote["description"]}")
+      st.write(f"Price: {quote["price"]}")  
