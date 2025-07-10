977fa1137aaa1615c59062fddacf181066139e99
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-10 10:33:55 +0200

    feat: Implement return of all quotes

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from database import get_db, engine
-from models import Base, Quote
+from models import Base, QuoteModel
 
 class Quote(BaseModel):
-  id: int
-  customer: str
-  description: str
-  price: int
+    id: int
+    customer: str
+    description: str
+    price: int
+
+    class Config:
+        orm_mode = True
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,8 +26,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/quotes")
-def get_all_quotes():
-  return {
-    "Message": "This will return all quotes"
-  }
+@app.get("/quotes", response_model=List[Quote])
+async def get_all_quotes(db: AsyncSession = Depends(get_db)):
+  result = await db.execute(select(QuoteModel))
+  quotes = result.scalars().all()
+  return quotes
