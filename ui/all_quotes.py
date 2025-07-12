a5c282e5d74e9e8b23ee8799d8f572f506f01a44
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-12 19:56:57 +0200

    fix: Move all quotes into own logic

--- ui/all_quotes.py
+++ ui/all_quotes.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import requests
 
 st.header("All Quotes:")
 
-
+with st.spinner():
+  response = requests.get("http://127.0.0.1:8000/quotes")
+  if response.status_code == 200:
+    quotes = response.json()
+    for quote in quotes:
+      st.divider()
+      st.header(f"Customer: {quote["customer"]}")
+      st.write(f"Description: {quote["description"]}")
+      st.write(f"Price: {quote["price"]}")
