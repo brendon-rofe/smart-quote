2d67e6d655ea98ff9c05e34162599de4f8b654bd
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-12 20:02:20 +0200

    feat: Setup initial create quote form

--- ui/create_quote.py
+++ ui/create_quote.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.header("Create A Quote:")
+
+with st.form("create_quote_form"):
+  st.text_input("Enter customer")
+  st.text_area("Enter description")
+  st.number_input("Enter a price")
+  st.form_submit_button("Create Quote")
