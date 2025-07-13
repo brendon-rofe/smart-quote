9f16c4e7cff8f7dd88ecadde739dd6369acd7721
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-13 14:32:18 +0200

    feat: Implement quote create form

--- ui/create_quote.py
+++ ui/create_quote.py
@@ -1,9 +1,24 @@
 import streamlit as st
+import requests
 
 st.header("Create A Quote:")
 
 with st.form("create_quote_form"):
-  st.text_input("Enter customer")
-  st.text_area("Enter description")
-  st.number_input("Enter a price")
-  st.form_submit_button("Create Quote")
+  customer = st.text_input("Enter customer")
+  description =  st.text_area("Enter description")
+  price = st.number_input("Enter a price")
+  submit = st.form_submit_button("Create Quote")
+  
+  if submit:
+    data = {
+      "customer": customer,
+      "description": description,
+      "price": price,
+    }
+    response = requests.post(
+      "http://127.0.0.1:8000/quotes",
+      json = data
+    )
+    if response.status_code == 200:
+      st.success("Quote created!")
+    
