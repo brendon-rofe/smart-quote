ee77b85df84750401f020ea9f34864e3792aabe1
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-14 13:36:09 +0200

    feat: Setup initial edit and delete quote functionality

--- ui/all_quotes.py
+++ ui/all_quotes.py
@@ -7,8 +7,15 @@ with st.spinner():
   response = requests.get("http://127.0.0.1:8000/quotes")
   if response.status_code == 200:
     quotes = response.json()
-    for quote in quotes:
+    for i, quote in enumerate(quotes):
       st.divider()
       st.header(f"Customer: {quote["customer"]}")
       st.write(f"Description: {quote["description"]}")
       st.write(f"Price: {quote["price"]}")
+      col1, col2, _ = st.columns([1, 1, 3])
+      
+      with col1:
+        st.button("Edit Quote", key=f"button1_{i}")
+
+      with col2:
+          st.button("Delete Quote", type="primary", key=f"button2_{i}")
