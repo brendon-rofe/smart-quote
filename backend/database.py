183a26b9857a61d524ce3658f3bbd046c5d4e4be
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-16 10:30:33 +0200

    feat: Update to throw 404 and update db creds

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite+aiosqlite:///./smart-quotes.db"
+DATABASE_URL = "mysql+aiomysql://user:dpassword@localhost:3306/smart_quote"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
