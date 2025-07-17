cf28630d591686d0de0519d98db5ca5bd29dca30
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-17 17:34:43 +0200

    feat: Implement initial chat functionality

--- ui/customer/request_quote.py
+++ ui/customer/request_quote.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import requests
 
-st.title("Customer Request a Quote")
-st.header("Coming soon...")
+def response_generator(prompt):
+  data = { "prompt": prompt }
+  response = requests.post(
+    "http://127.0.0.1:8000/agent",
+    json = data
+  )
+  return response.json()
+
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  with st.chat_message("user"):
+    st.markdown(prompt)
+
+  with st.chat_message("assistant"):
+    response = st.write(response_generator(prompt))
+  st.session_state.messages.append({"role": "assistant", "content": response})
