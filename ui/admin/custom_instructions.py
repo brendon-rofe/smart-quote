e64b958da30dccdcd2633e6d4874f78069a1ecaa
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-22 12:27:09 +0200

    feat: Add custom instructions

--- ui/admin/custom_instructions.py
+++ ui/admin/custom_instructions.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 with st.form("custom_instructions_form"):
   custom_instructions = st.text_area("Enter your custom instructions here...")
@@ -6,4 +7,15 @@ with st.form("custom_instructions_form"):
   
 
 if submit:
-  st.success(custom_instructions)
+  data = {
+      "custom_instructions": custom_instructions,
+    }
+  with st.spinner("Creating quote..."):
+    response = requests.post(
+      "http://127.0.0.1:8000/custom-instructions",
+      json = data
+    )
+  if response.status_code == 200:
+    st.success("✅ Your custom instructions have been saved!")
+  else:
+    st.error(f"❌ Failed to save custom instructions: {response.text}")
