7e6e130311931b1667dc7515807dcf221711dc5d
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-24 14:01:45 +0200

    feat: Initial customer quotes setup

--- ui/customer/customer_quotes.py
+++ ui/customer/customer_quotes.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title("All Customer Quotes")
-st.header("Coming soon...")
+st.title("All Your Quotes")
+
+st.divider()
+st.write(f"Description: Description goes here...")
+st.write(f"Price: Price goes here...")
+st.divider()
