834a6a02bc34dcf5246fb654f88ea94b01687afd
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-26 18:03:53 +0200

    feat: display all of specific customer's quotes

--- ui/customer/customer_quotes.py
+++ ui/customer/customer_quotes.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import requests
 
-st.title("All Your Quotes")
+st.title("All your quotes:")
 
-st.divider()
-st.write(f"Description: Description goes here...")
-st.write(f"Price: Price goes here...")
-st.divider()
+def display_all_customer_quotes(customer_name):
+  response = requests.get(f"http://127.0.0.1:8000/quotes/{customer_name}")
+  if response.status_code == 404:
+    st.warning("🤷‍♂️ You don't seem to be any quotes at the moment...")
+  if response.status_code == 200:
+    customer_quotes = response.json()
+    print("Customer quotes:", customer_quotes)
+    for quote in customer_quotes:
+      st.divider()
+      st.write(f"Description: {quote['description']}")
+      st.write(f"Price: {quote['price']}")
+      
+display_all_customer_quotes(st.session_state.customer_name)
