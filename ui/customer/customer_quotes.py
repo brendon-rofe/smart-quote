a398590bd71b808a2f7af6e24f3b48d7324f34b5
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-28 11:09:45 +0200

    feat: Create initial customer quote delete functionality

--- ui/customer/customer_quotes.py
+++ ui/customer/customer_quotes.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import requests
+import time
+
+if "deleting_quote" not in st.session_state:
+  st.session_state.deleting_quote = None
 
 st.title("All your quotes:")
 
@@ -9,9 +13,18 @@ def display_all_customer_quotes(customer_name):
     st.warning("🤷‍♂️ You don't seem to be any quotes at the moment...")
   if response.status_code == 200:
     customer_quotes = response.json()
-    for quote in customer_quotes:
+    for i, quote in enumerate(customer_quotes):
       st.divider()
       st.write(f"Description: {quote['description']}")
       st.write(f"Price: {quote['price']}")
+      
+      if st.button("Delete Quote", type="primary", key=f"{i}"):
+        response = requests.delete(f"http://127.0.0.1:8000/quotes/{quote['id']}")
+        st.session_state.deleting_quote = i
+        if response.status_code == 204:
+          st.success("Quote deleted successfully!")
+          time.sleep(1)
+          st.session_state.deleting_quote = None
+          st.rerun()
       
 display_all_customer_quotes(st.session_state.customer_name)
