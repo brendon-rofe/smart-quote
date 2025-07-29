d1e0dc9c1c17b047dba82d66568162648c371651
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-29 14:25:46 +0200

    feat: Implement check before deleteing customer quote

--- ui/customer/customer_quotes.py
+++ ui/customer/customer_quotes.py
@@ -18,12 +18,24 @@ def display_all_customer_quotes(customer_name):
       st.write(f"Description: {quote['description']}")
       st.write(f"Price: {quote['price']}")
       
-      if st.button("Delete Quote", type="primary", key=f"{i}"):
-        response = requests.delete(f"http://127.0.0.1:8000/quotes/{quote['id']}")
+      if st.button("Delete Quote", type="primary", key=f"button2_{i}"):
         st.session_state.deleting_quote = i
-        if response.status_code == 204:
-          st.success("Quote deleted successfully!")
-          time.sleep(1)
+      if st.session_state.deleting_quote == i:
+        st.warning("Are you sure you want to delete this quote?")
+        col_yes, col_no = st.columns(2)
+        yes = col_yes.button("Yes")
+        no = col_no.button("No")
+        if yes:
+          with st.spinner("Deleting quote..."):
+            response = requests.delete(f"shttp://127.0.0.1:8000/quotes/{quote['id']}")
+          if response.status_code == 204:
+            st.success("Quote deleted successfully!")
+            time.sleep(1)
+            st.session_state.deleting_quote = None
+            st.rerun()
+          else:
+            st.error("Failed to delete quote")
+        if no:
           st.session_state.deleting_quote = None
           st.rerun()
       
