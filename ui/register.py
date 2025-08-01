125806a9ef7128224b18f1577ac012e8bceb4c6a
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-01 15:36:33 +0200

    feat: Create registration form

--- ui/register.py
+++ ui/register.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Register")
+
+with st.form("registration_form"):
+  st.text_input("username")
+  st.text_input("email")
+  st.text_input("password")
+  st.text_input("role")
+  st.form_submit_button("Register")
