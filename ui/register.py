5fad1513231d241fbd63abce74c422cbe47814d9
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-03 21:29:29 +0200

    fix: Hard set role as customer

--- ui/register.py
+++ ui/register.py
@@ -7,7 +7,6 @@ with st.form("registration_form"):
   user_username = st.text_input("username")
   user_email = st.text_input("email")
   user_password = st.text_input("password")
-  user_role = st.text_input("role")
   submit = st.form_submit_button("Register")
   
   if submit:
@@ -15,7 +14,7 @@ with st.form("registration_form"):
       "username": user_username,
       "email": user_email,
       "password": user_password,
-      "role": user_role
+      "role": "customer"
     }
     response = requests.post(
       "http://127.0.0.1:8000/users",
