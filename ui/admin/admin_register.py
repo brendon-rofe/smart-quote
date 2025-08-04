2dc16fade8e1e3eb3447969f91d809b6102795d1
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-04 09:35:35 +0200

    feat/fix: Move and rename customer_register page. Create admin register page.

---
+++ ui/admin/admin_register.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+st.title("Register New Admin User")
+
+with st.form("registration_form"):
+  user_username = st.text_input("username")
+  user_email = st.text_input("email")
+  user_password = st.text_input("password")
+  submit = st.form_submit_button("Register")
+  
+  if submit:
+    data = {
+      "username": user_username,
+      "email": user_email,
+      "password": user_password,
+      "role": "admin"
+    }
+    response = requests.post(
+      "http://127.0.0.1:8000/users",
+      json = data
+    )
+    if response.status_code == 200:
+      st.success("You have successfully been registered!")
+    else:
+      st.error("Failed to register")
+
